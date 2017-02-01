0946c0d3252978500bb07cdb51737839ce06fdec
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-01 10:40:07 -0500

    Cleaned up nav bar

--- resources/views/includes/navBar_include.blade.php
+++ resources/views/includes/navBar_include.blade.php
@@ -8,49 +8,31 @@
     @yield('header')
 </header>
 <div id="navBar">
-    <div id="navButtonsDiv">
-        <button class="navButton @yield('resultsClassDefault')" id="resultsNavButton">Results</button>
-        <button class="navButton @yield('campaignsClassDefault')" id="campaignsNavButton">Campaigns</button>
-        <button class="navButton @yield('templatesClassDefault')" id="templatesNavButton">Templates</button>
-        <button class="navButton @yield('emailClassDefault')" id="emailNavButton">Email</button>
-    </div>
-    @if(\Session::get('authUser'))
+    @if(\Session::get('authUser') || \Session::get('adminUser'))
+        <ul>
+            <li class="dropdown">
+                <a href="#reports" class="dropbtn">Reports</a>
+                <div class="navbar-dropdown">
+                    <a href="#reports_web">Web</a>
+                    <a href="#reports_email">Email</a>
+                </div>
+            </li>
+            <li><a href="#campaigns">Campaigns</a></li>
+            <li><a href="#templates">Templates</a></li>
+            <li class="dropdown">
+                <a href="#mlu" class="dropbtn">Mailing Lists</a>
+                <div class="navbar-dropdown">
+                    <a href="#mlu_manage">Manage</a>
+                    <a href="#mlu_groups">Groups</a>
+                </div>
+            </li>
+            @if(\Session::get('adminUser'))
+                <li><a href="#users">Users</a></li>
+            @endif
+        </ul>
         <a class="authenticationLink" target="_blank" href="#">MY ACCOUNT</a> <!-- create a my account page -->
         <a class="authenticationLink" href="/logout">LOGOUT</a>
     @else
         <a class="authenticationLink" href="/login">LOGIN</a>
     @endif
-    <div id="navInfoDiv">
-        <div class="templatesInfo">
-            <img class='scalable' src="/images/thumbnails/templatePaper.png" /><h2><a href="/templates/show">View Templates</a></h2>
-            <p>View all the templates currently accessible through the interface.</p>
-        </div>
-        <div class="templatesInfo">
-            <img class='scalable' src="/images/thumbnails/createNewTemplate.png" /><h2><a href="/templates/create">New Template</a></h2>
-            <p>Create a New Template to be used in the interface.</p>
-        </div>
-        <div class="campaignsInfo">
-            <img class='scalable' src="/images/thumbnails/templatePaper.png" /><h2><a href="/campaigns/show">View Campaigns</a></h2>
-            <p>View all the campaigns, both active and inactive.</p>
-        </div>
-        <div class="campaignsInfo">
-            <img class='scalable' src="/images/thumbnails/createNewCampaign.png" /><h2><a href="/campaigns/create">New Campaign</a></h2>
-            <p>Create a New Campaign to be used in managing phishing email scams.</p>
-        </div>
-        <div class="emailInfo">
-            <img class='scalable' src="/images/thumbnails/createNewEmail.png" /><h2><a href="/email/generate">Start Campaign</a></h2>
-            <p>Start a new phishing campaign or add to a current one.</p>
-        </div>
-        <div class="emailInfo">
-            <img class='scalable' src="/images/thumbnails/emailSettings.png" /><h2><a href="/email/settings">Settings</a></h2>
-            <p>Set default settings for your email environment.</p>
-        </div>
-        <div class="emailInfo">
-            <img class='scalable' src="/images/thumbnails/newUser.png" /><h2><a href="/mailinglist/create/user">Create Mailing List User</a></h2>
-            <p>Create a New Mailing List User to include in new campaigns.</p>
-        </div>
-        <div class="authInfo">
-            <p>Please login to access information on this site.</p>
-        </div>
-    </div>
 </div>
