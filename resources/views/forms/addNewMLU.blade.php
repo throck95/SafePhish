1f006e2ace6ea9e4e77809f18f2d05e8715cbab5
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-05 01:27:31 -0500

    Cleaned up template name length

--- resources/views/forms/addNewMLU.blade.php
+++ resources/views/forms/addNewMLU.blade.php
@@ -6,18 +6,13 @@
     <meta name="_token" content="{{ csrf_token() }}" />
 @stop
 @section('scripts')
-    <script type="text/javascript" src="/js/emailRequirements.js"></script>
+
 @stop
 @section('formcss')
-    <link rel="stylesheet" type="text/css" href="/css/baseformstyles.css" />
-@stop
-@section('emailClassDefault')
-    activeNavButton tempActiveNavButton
+
 @stop
 @section('bodyContent')
     {!! Form::open(array('route'=>'postMailingListUser')) !!}
-    <p>{!! Form::label('usernameText','Username: ') !!}
-        <input type="text" id="usernameText" name="usernameText" /></p>
     <p>{!! Form::label('emailText','Email Address: ') !!}
         <input type="text" id="emailText" name="emailText" /></p>
         <p>{!! Form::label('firstNameText','First Name: ') !!}
@@ -25,15 +20,11 @@
     <p>{!! Form::label('lastNameText','Last Name: ') !!}
         <input type="text" id="lastNameText" name="lastNameText" /></p>
     <p>{!! Form::label('departmentSelect','Department: ') !!}
-        <select id='departmentSelect' name='departmentSelect'>
-            <option value="-1">--Default--</option>
+        <select id='departmentSelect' name='departmentSelect[]' multiple>
             @for ($i = 0; $i < count($departments); $i++)
                 <option value="{{ $departments[$i]->Id }}">{{ $departments[$i]->Department }}</option>
             @endfor
-            <option value="0">Create New</option>
         </select></p>
-    <p>{!! Form::label('createNewDepartmentText','New Department: ',array('style'=>'visibility: visible')) !!}
-        <input type="text" id="createNewDepartmentText" name="createNewDepartmentText" style="visibility: visible"/></p>
     <br /><br />
     {!! Form::submit('Submit',array('id'=>'submitButton')) !!}
     {!! Form::close() !!}
