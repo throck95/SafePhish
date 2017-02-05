843ed96b83d29b5f66a6ca57367d0dbdeecf3f06
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-05 01:49:00 -0500

    Updated create campaign view name

---
+++ resources/views/forms/createCampaign.blade.php
@@ -0,0 +1,28 @@
+@extends('masters.basemaster')
+@section('title')
+    Create New Campaign
+@stop
+@section('csrf_token')
+    <meta name="_token" content="{{ csrf_token() }}" />
+@stop
+@section('scripts')
+
+@stop
+@section('bodyContent')
+    <h3 style="font-weight: 300">Create New Campaign</h3>
+    {!! Form::open(array('action'=>'GUIController@createNewCampaign')) !!}
+    <datalist id="usersDatalist">
+        @for ($i = 0; $i < count($users); $i++)
+            <option value="{{ $users[$i]->Id }}">
+                {{ $users[$i]->FirstName }} {{ $users[$i]->LastName }}</option>
+        @endfor
+    </datalist>
+    <p>{!! Form::label('nameText','Name: ') !!}
+        {!! Form::text('nameText',null,array('name'=>'nameText')) !!}</p>
+    <p>{!! Form::label('descriptionText','Description: ') !!}
+        <textarea id="descriptionText" name="descriptionText" rows="4" cols="50"></textarea></p>
+    <p>{!! Form::label('assigneeText','Campaign Assignee: ') !!}
+        {!! Form::text('assigneeText',null,array('name'=>'assigneeText','list'=>'usersDatalist')) !!}</p>
+    {!! Form::submit('Submit',array('id'=>'submitButton')) !!}
+    {!! Form::close() !!}
+@stop
