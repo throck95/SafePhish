3a7c2e52d6e4b2e0896d997aeb69b0a4a09123f2
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-07 18:12:13 -0500

    Began implementing Mailables

---
+++ app/Mail/AdminForcedPasswordReset.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AdminForcedPasswordReset extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $user;
+    private $password;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, $password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $variables = array('user'=>$this->user,'password'=>$this->password);
+        return $this->view('emails.resetPassword')->with($variables);
+    }
+}
---
+++ app/Mail/TwoFactorCode.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TwoFactorCode extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $user;
+    private $code;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, $code)
+    {
+        $this->user = $user;
+        $this->code = $code;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $variables = array('user'=>$this->user,'securityCode'=>$this->code);
+        return $this->view('emails.2fa')->with($variables);
+    }
+}
