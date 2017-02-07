7e3f7bbce9da4a3da7af978dfe189b4789bd3c12
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-06 21:12:09 -0500

    Created email template for when a new user is registered

---
+++ resources/views/emails/newUser.blade.php
@@ -0,0 +1,18 @@
+<p>Dear {{ $firstName }} {{ $lastName }},</p>
+<p>Your company has created you an account at <a href="http://www.SafePhish.com">SafePhish.com</a>. Here at SafePhish,
+    our goal is to help you help yourself. Phishing is becoming a larger problem every day. Now you can help change that.</p>
+<p>Log in to your SafePhish account using the password below. Don't forget to change your password after you log in!</p>
+<p>{{ $password }}</p>
+<p>Once you've logged in, start by looking around, then start some campaigns. See if you can help your coworkers understand
+    what's so dangerous about phishing.</p>
+<p>SafePhish will never send an email requesting information that will uniquely identify you. If you feel you have
+    received a suspicious email, please forward it to our <a href="mailto:phishing@safephish.com">Phishing Response Team</a>.</p>
+
+<br /><br />
+<p>This message, including any attachments, may contain information
+    which is confidential and privileged. Unless you are the addressee (or
+    authorized to receive for the addressee), you may not read, use, copy
+    or disclose to anyone the message, its attachments, or any information
+    contained therein. If you have received the message in error, please
+    advise the sender by reply e-mail and delete the message and any
+    attachments.</p>
