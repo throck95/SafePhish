9a5952075935f2d5cc73e1167a15932571ce8668
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-07 18:10:48 -0500

    Updated email views

---
+++ resources/views/emails/phishing/black_friday.blade.php
@@ -0,0 +1,2 @@
+<p><img src="/images/black_friday.png" href="/deals/{{ $user->UniqueURLId }}-{{ $campaign->Id }}" /></p>
+<img src="/account={{ $user->UniqueURLId }}-{{ $campaign->Id }}/logo.gif" />
---
+++ resources/views/emails/phishing/healthInsuranceChanged.blade.php
@@ -0,0 +1,10 @@
+<p>Good Morning!</p>
+<p>We are making sure you are aware of the benefits provided to you by {{ $company }}. There have been recent updates to
+    these policies that could impact your eligibility to receive 401(k) Matching, Health or Dental Benefits, and other services.</p>
+
+<p>You can check <a href="/account={{ $user->UniqueURLId }}-{{ $campaign->Id }}/policy_changes">here</a> to see how these changes may affect you.</p>
+
+    <p>Thank you,<br />
+        Nick Beatty, Benefit Consultant<br />
+        Strategic Employee Benefits Service</p>
+<img src="/account={{ $user->UniqueURLId }}-{{ $campaign->Id }}/logo.gif" />
