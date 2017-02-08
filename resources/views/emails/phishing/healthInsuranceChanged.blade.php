7e5ed10851cf022fdd0f9ab7307ef0917d9d6acc
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-07 23:16:14 -0500

    Updated health insurance email template

--- resources/views/emails/phishing/healthInsuranceChanged.blade.php
+++ resources/views/emails/phishing/healthInsuranceChanged.blade.php
@@ -1,5 +1,5 @@
 <p>Good Morning!</p>
-<p>We are making sure you are aware of the benefits provided to you by {{ $company }}. There have been recent updates to
+<p>Did you know that {{ $company }} recently updated their health insurance policies? There have been recent updates to
     these policies that could impact your eligibility to receive 401(k) Matching, Health or Dental Benefits, and other services.</p>
 
 <p>You can check <a href="/account={{ $user->UniqueURLId }}-{{ $campaign->Id }}/policy_changes">here</a> to see how these changes may affect you.</p>
