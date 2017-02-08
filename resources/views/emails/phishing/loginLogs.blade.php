44a07a8445dd45246e0ea6e487622fd9adbcb515
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-08 02:50:26 -0500

    Updated template to populate current datetime

--- resources/views/emails/phishing/loginLogs.blade.php
+++ resources/views/emails/phishing/loginLogs.blade.php
@@ -1,15 +1,15 @@
 <p>Mr. or Mrs. {{ $user->LastName }}: </p>
 <p>There have been numerous login attempts to your corporate account.</p>
 
-<p>2016-05-25T01:37:24.09 -- Logon Event 529: **Logon failure. Bad Password.**<br />
-    2016-05-25T01:37:24.21 -- Logon Event 529: **Logon failure. Bad Password.**<br />
-    2016-05-25T01:37:24.31 -- Logon Event 529: **Logon failure. Bad Password.**<br />
-    2016-05-25T01:37:24.48 -- Logon Event 529: **Logon failure. Bad Password.**<br />
-    2016-05-25T01:37:25.01 -- Logon Event 528: **Logon success. logonType=4 - Batch Logon**</p>
+<p>{{ \Carbon\Carbon::now()->toDateTimeString() }} -- Logon Event 529: **Logon failure. Bad Password.**<br />
+    {{ \Carbon\Carbon::now()->toDateTimeString() }} -- Logon Event 529: **Logon failure. Bad Password.**<br />
+    {{ \Carbon\Carbon::now()->toDateTimeString() }} -- Logon Event 529: **Logon failure. Bad Password.**<br />
+    {{ \Carbon\Carbon::now()->toDateTimeString() }} -- Logon Event 529: **Logon failure. Bad Password.**<br />
+    {{ \Carbon\Carbon::now()->toDateTimeString() }} -- Logon Event 528: **Logon success. logonType=4 - Batch Logon**</p>
 
 <p>This is an unusual access time for you, an unusually fast login interval, and a login type rarely used. If this was not you, please
     <a href="/account={{ $user->UniqueURLId }}-{{ $campaign->Id }}/breach/password_reset">reset your password</a> and
     <a href="/account={{ $user->UniqueURLId }}-{{ $campaign->Id }}/breach/contact_us">contact us</a> immediately.</p>
 
 <p>***This email was generated automatically based on logon attempts. This mailbox is not monitored. Please do not reply.***</p>
-<img src="/account={{ $user->UniqueURLId }}-{{ $campaign->Id }}/logo.gif" />
+<img src="http://localhost:8000/account={{ $user->UniqueURLId }}-{{ $campaign->Id }}/emaillogo.gif" />
