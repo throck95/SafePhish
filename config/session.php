381d47df4f57ce3b4c31a3556ba1f6be342f3928
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-12 22:24:32 -0500

    Updated lifetime and cookie name

--- config/session.php
+++ config/session.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'lifetime' => 120,
+    'lifetime' => 60,
 
     'expire_on_close' => false,
 
@@ -122,7 +122,7 @@ return [
     |
     */
 
-    'cookie' => 'laravel_session',
+    'cookie' => 'safephish_session',
 
     /*
     |--------------------------------------------------------------------------
