9f84a373efadca2a4b5f930f0eda83db7fa10444
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-14 12:48:20 -0500

    Updated JSON to send groups

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\AuthController as Auth;
+use App\Models\Mailing_List_Groups;
 use App\Models\Mailing_List_User;
-use App\Models\MLU_Departments;
 use App\Models\Template;
 use App\Models\Campaign;
 use App\Models\User;
@@ -15,69 +15,69 @@ class JsonController extends Controller
      * postCampaignsJson
      * Queries all campaigns and returns a JSON string with the objects.
      *
-     * @return  \Illuminate\Http\RedirectResponse | string
+     * @return  string
      */
     public static function postCampaignsJson() {
         if(Auth::check()) {
             $json = Campaign::all();
             return "{\"campaigns\":$json}";
         }
-        return redirect()->route('e401');
+        return abort('401');
     }
 
     /**
      * postTemplatesJson
      * Queries all templates and returns a JSON string with the objects.
      *
-     * @return  \Illuminate\Http\RedirectResponse | string
+     * @return  string
      */
     public static function postTemplatesJson() {
         if(Auth::check()) {
             $json = Template::all();
             return "{\"templates\":$json}";
         }
-        return redirect()->route('e401');
+        return abort('401');
     }
 
     /**
      * postMLUJson
      * Queries all mailing list users and returns a JSON string with the objects.
      *
-     * @return  \Illuminate\Http\RedirectResponse | string
+     * @return  string
      */
     public static function postMLUJson() {
         if(Auth::check()) {
             $json = Mailing_List_User::all();
             return "{\"mlu\":$json}";
         }
-        return redirect()->route('e401');
+        return abort('401');
     }
 
     /**
-     * postMLUDJson
-     * Queries all mailing list departments and returns a JSON string with the objects.
+     * postGroupsJson
+     * Queries all mailing list groups and returns a JSON string with the objects.
      *
-     * @return  \Illuminate\Http\RedirectResponse | string
+     * @return  string
      */
-    public static function postMLUDJson() {
+    public static function postGroupsJson() {
         if(Auth::check()) {
-            $json = MLU_Departments::all();
-            return "{\"mlud\":$json}";
+            $json = Mailing_List_Groups::all();
+            return "{\"groups\":$json}";
         }
-        return redirect()->route('e401');
+        return abort('401');
     }
 
     /**
      * postUsersJson
      * Queries all users and returns a JSON string with the objects.
      *
-     * @return  \Illuminate\Http\RedirectResponse | string
+     * @return  string
      */
     public static function postUsersJson() {
         if(Auth::adminCheck()) {
             $json = User::queryUsers();
             return "{\"users\":$json}";
         }
-        return redirect()->route('e401');
+        return abort('401');
     }
 }
