0c5ea513bc08673fd60da918f3eafd1d8c8700b0
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-19 04:08:01 -0500

    Added 403 error page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,18 @@
+@extends('masters.basemaster')
+@section('title')
+    Forbidden
+@stop
+@section('scripts')
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+@stop
+@section('stylesheets')
+
+@stop
+@section('bodyContent')
+    <h3 style="text-align: center; font-weight: 300">Woops! You do not have permission to view this page.</h3>
+    <br />
+@stop
+@section('footer')
+    <p></p>
+@stop
