07c8f04ed5bad005dc15ab5a9c97fca22e9d4994
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-21 16:24:55 -0500

    Added admin company viewing

--- resources/views/displays/showAllCampaigns.blade.php
+++ resources/views/displays/showAllCampaigns.blade.php
@@ -28,12 +28,18 @@
             <th ng-click='sortColumn("Name")' ng-class='sortClass("Name")'>Name</th>
             <th ng-click='sortColumn("Description")' ng-class='sortClass("Description")'>Description</th>
             <th ng-click='sortColumn("Status")' ng-class='sortClass("Status")'>Status</th>
+            @if(\App\Http\Controllers\AuthController::safephishAdminCheck())
+                <th ng-click='sortColumn("CompanyName")' ng-class='sortClass("CompanyName")'>Company Name</th>
+            @endif
             <th ng-click='sortColumn("Created")' ng-class='sortClass("Created")'>Created</th>
             <th ng-click='sortColumn("Updated")' ng-class='sortClass("Updated")'>Updated</th>
             <tr ng-repeat="x in campaigns | orderBy:column:reverse | filter:search | filter:buttonSearch:exceptEmpty">
                 <td>[[ x.name ]]</td>
                 <td>[[ x.description ]]</td>
                 <td>[[ x.status ]]</td>
+                @if(\App\Http\Controllers\AuthController::safephishAdminCheck())
+                    <td>[[ x.company_name ]]</td>
+                @endif
                 <td>[[ x.created_at ]]</td>
                 <td>[[ x.updated_at ]]</td>
                 <td><a ng-href='/campaigns/[[ x.Id ]]'>Edit</a></td>
--- resources/views/displays/showAllMailingListGroups.blade.php
+++ resources/views/displays/showAllMailingListGroups.blade.php
@@ -23,8 +23,14 @@
         </div>
         <table>
             <th ng-click='sortColumn("Name")' ng-class='sortClass("Name")'>Name</th>
+            @if(\App\Http\Controllers\AuthController::safephishAdminCheck())
+                <th ng-click='sortColumn("CompanyName")' ng-class='sortClass("CompanyName")'>Company Name</th>
+            @endif
             <tr ng-repeat="x in groups | orderBy:column:reverse | filter:search">
                 <td>[[ x.name ]]</td>
+                @if(\App\Http\Controllers\AuthController::safephishAdminCheck())
+                    <td>[[ x.company_name ]]</td>
+                @endif
                 <td><a ng-href='/mailinglist/update/group/[[ x.id ]]'>Edit</a></td>
                 <!--<td><a ng-href="#delete">Delete</a></td>-->
             </tr>
