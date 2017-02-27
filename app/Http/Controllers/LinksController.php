a0c5413a4bcd1a6ef8697b6a32d0bbbd065ff57e
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-02-27 04:16:57 -0500

    Updated disclosure

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Email_Tracking;
-use App\Models\Website_Tracking;
-use App\Http\Controllers\AuthController As Auth;
-use Illuminate\Http\Request;
+use App\Models\Company;
+use App\Models\Mailing_List_User;
 
 class LinksController extends Controller
 {
-    public static function disclosePhishingEmail() {
-        return view('links.disclosure');
+    public static function disclosePhishingEmail($id) {
+        $tipsVideoUrl = getenv('TIPS_VIDEO_URL');
+        $urlId = substr($id,0,intval(getenv('DEFAULT_LENGTH_IDS')));
+        $mailing_list_user = Mailing_List_User::where('unique_url_id',$urlId)->first();
+
+        if(empty($mailing_list_user)) {
+            abort('500');
+        }
+
+        $company = Company::where('id',$mailing_list_user->company_id)->first();
+
+        if(empty($company)) {
+            abort('500');
+        }
+
+        $variables = array('tipsVideoUrl'=>$tipsVideoUrl,'company'=>$company->name);
+        return view('links.disclosure')->with($variables);
     }
 }
--- resources/views/links/disclosure.blade.php
+++ resources/views/links/disclosure.blade.php
@@ -1,9 +1,17 @@
 @extends('masters.basemaster')
 @section('title')
-    Phishing Emails
+    Uh-Oh! This was a Phish!
 @stop
 @section('bodyContent')
 <div>
-    <p>This was a phishing email.</p>
+    <p>Uh-oh! This was a phishing email. Your company, {{ $company }}, wants to help you learn how to identify phishing emails and deal with them.</p>
+    <p>Phishing is becoming a much larger problem every day. Here's some tips that you can use to help you identify a phish:</p>
+    <ol>
+        <li>Do you know the sender? If not, it's likely a phish.</li>
+        <li>Is the email asking for information? If so, it's likely a phish.</li>
+        <li>Does the email contain numerous links? If so, it's likely a phish.</li>
+    </ol>
+    <p>In general, trust your instincts. Common sense is your best ally when dealing with phishing scams.</p>
+    <iframe width="600" height="338" src="{{ $tipsVideoUrl }}" frameborder="0" allowfullscreen></iframe>
 </div>
 @stop
