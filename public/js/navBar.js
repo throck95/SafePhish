7e74649b54c48f788261c150bd1cf0915aaee4b1
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-03-03 01:26:35 -0500

    Moved coypright info out of navBar.js

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -1,14 +1,8 @@
 $('document').ready(function() {
-    var currentYear = (new Date).getFullYear();
-    $('footer>#copyright').html("&copy; " + currentYear + " SafePhish. All Rights Reserved");
-    assignAuthLinkPositions();
-});
-
-function assignAuthLinkPositions() {
     var counter = 0;
     $('.authenticationLink').each(function() {
         var pixel = counter*10;
         $(this).css('right',pixel+'px');
         counter++;
     });
-}
+});
