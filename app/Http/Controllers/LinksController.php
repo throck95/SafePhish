e780f1675e2c3b2a203ce0fdc5b1db757d3f6cce
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-03-29 01:43:17 -0400

    Updated disclosure server logic

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Campaign;
 use App\Models\Company;
 use App\Models\Mailing_List_User;
 
@@ -10,19 +11,28 @@ class LinksController extends Controller
     public static function disclosePhishingEmail($id) {
         $tipsVideoUrl = getenv('TIPS_VIDEO_URL');
         $urlId = substr($id,0,intval(getenv('DEFAULT_LENGTH_IDS')));
-        $mailing_list_user = Mailing_List_User::where('unique_url_id',$urlId)->first();
+        $campaignId = substr($id,intval(getenv('DEFAULT_LENGTH_IDS'))+1);
+        $user = Mailing_List_User::where('unique_url_id',$urlId)->first();
 
-        if(empty($mailing_list_user)) {
+        if(empty($user)) {
             abort('500');
         }
 
-        $company = Company::where('id',$mailing_list_user->company_id)->first();
+        $campaign = Campaign::where('id',$campaignId)->first();
+
+        if(empty($campaign)) {
+            abort('500');
+        }
+
+        $company = Company::where('id',$user->company_id)->first();
 
         if(empty($company)) {
             abort('500');
         }
 
-        $variables = array('tipsVideoUrl'=>$tipsVideoUrl,'company'=>$company->name);
+        WebbugController::webbugExecutionWebsite($user,$campaign);
+
+        $variables = array('tipsVideoUrl'=>$tipsVideoUrl,'company'=>$company->name,'user'=>$user,'campaign'=>$campaign);
         return view('links.disclosure')->with($variables);
     }
 }
