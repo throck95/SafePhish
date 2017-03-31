126326262902ddc40eae090eaf16108cdb818157
Tyler Throckmorton <tylerthrock95@gmail.com>
2017-03-31 02:29:05 -0400

    Updated link in view

--- resources/views/displays/documentation.blade.php
+++ resources/views/displays/documentation.blade.php
@@ -4,7 +4,7 @@
 @stop
 @section('bodyContent')
     <h3 style="text-align: center; font-weight: 300">We are actively working on creating comprehensive documentation to help you better understand how our tool works and what to do to setup the system.
-        If you have any suggestions, send them to <a href="mailto:tyler@safephish.org">Tyler.</a></h3>
+        If you have any suggestions, send them to <a href="mailto:tyler@safephish.org?Subject=Documentation%20Suggestion" target="_top">Tyler.</a></h3>
     <br />
 @stop
 @section('footer')
